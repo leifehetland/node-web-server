b3c520b0039d630ad431a10b4f7aa6dab9f7d28b
Leif Hetland <lehetland1@gmail.com>
2018-08-08 00:31:29 -0400

    weird package.json issue, wasn't saving handlebars, added project template page

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Project Page',
+		welcomeMessage: 'Projects will go here!'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage
