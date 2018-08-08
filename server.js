0f935daa9e14a8731153b3aa8ea41cf43b433623
Leif Hetland <lehetland1@gmail.com>
2018-08-07 23:24:15 -0400

    express middleware exploration

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const fs = require('fs');
+
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log');
+		}
+	});
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
+// Handle Bar Helpers
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear();
 });
@@ -16,12 +35,13 @@ hbs.registerHelper('toUpperCase', (text) => {
 	return text.toUpperCase();
 });
 
+// Routes
 app.get('/', (req, res) => {
 	// res.send('<h1 style="color: blue; font-family: Roboto;">Hello Express.</h1>');
 	res.render('home.hbs', {
 		pageTitle: 'Home Page',
 		welcomeMessage: 'Welcome to the Home Page!'
-	})
+	});
 });
 
 app.get('/about', (req, res) => {
@@ -37,6 +57,7 @@ app.get('/bad', (req, res) => {
 	});
 });
 
+// Listen sets the port
 app.listen(3000, () => {
 	console.log('Server is up on port 3000.');
 });
