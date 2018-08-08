69bb8155df4a2e786b846a8e9ba82894ead853b8
Leif Hetland <lehetland1@gmail.com>
2018-08-07 22:57:33 -0400

    exploring handlebar helpers

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('toUpperCase', (text) => {
+	return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
 	// res.send('<h1 style="color: blue; font-family: Roboto;">Hello Express.</h1>');
 	res.render('home.hbs', {
 		pageTitle: 'Home Page',
-		currentYear: new Date().getFullYear(),
 		welcomeMessage: 'Welcome to the Home Page!'
 	})
 });
@@ -19,7 +27,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
 	res.render('about.hbs', {
 		pageTitle: 'About Page',
-		currentYear: new Date().getFullYear()
 	});
 });
 
